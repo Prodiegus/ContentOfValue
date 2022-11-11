7290eec65161e3e9ced213de2ec928b9e9059722
Diego Fernadez <dfernandez19@alumnos.utalca.cl>
2022-11-10 22:22:07 -0600

    Intento de conectar BD

---
+++ src/test.py
@@ -0,0 +1,1 @@
+cnx = mysql.connector.connect(user="ovni", password="ContentOfValue", host="contentofvalue.mysql.database.azure.com", port=3306, database="cov", ssl_ca="../certificacion para data base/DigiCertGlobalRootCA.crt.pem", ssl_disabled=False)
