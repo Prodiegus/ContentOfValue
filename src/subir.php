dd5e02d80cc5e79d9c124cc9892245b8ddbfcd54
Diego Fernadez <dfernandez19@alumnos.utalca.cl>
2022-11-30 16:12:51 -0600

    Testeando Base de datos

---
+++ src/subir.php
@@ -0,0 +1,39 @@
+<?php
+
+$con = mysqli_init(); 
+mysqli_ssl_set($con,NULL,NULL, "..\certificacion para data base\DigiCertGlobalRootCA.crt.pem", NULL, NULL); 
+mysqli_real_connect(
+    $conn, "contentofvalue.mysql.database.azure.com", 
+    "ovni", 
+    "ContentOfValue#2022", "cov", 3306, MYSQLI_CLIENT_SSL);
+
+if (!$con) {
+    echo "Error en conexión.";
+}
+
+$nombre = $_GET('Nombre')." ".$_GET('Apellido');
+$email = $_GET['Correo'];
+$query = "SELECT * FROM cliente";
+$resultado =mysqli_query($con, $query);
+
+foreach ($resultado as $columna){
+    echo $columna ['nombre'] . " ". $columna ['mail'];
+}
+
+/*
+if(isset($_POST['enviar'])){
+    if(!empty($_POST['nombre'])&&!empty($_POST['email'])){
+        $name = $_POST['nombre']." ".$_POST('Apellido');
+        $asunto = $_POST["Bienvenido a COV."];
+        $msg = $_POST["Bienvenido, nos da mucho gusto que nos hayas elejido."];
+        $email = $_POST['email'];
+        $header = "FROM: noreply@cov.com"."\r\n";
+        $header.= "Reply-To: noreply@cov.com"."\r\n";
+        $header.= "X-Mailer: PHP/".phpversion();
+        $mail = mail($email,$asunto,$msg,$header);
+        if($email){
+            echo "<h4>¡Mail enviado correctamente!</h4>";
+        }
+    }
+}*/
+?>
